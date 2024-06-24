c36d502bfed16da5b50ee9984c6792ef3f5623e2
angger <angger@newtronic-solution.com>
2024-06-24 20:00:05 +0700

    feature: add responsive page contact-us

--- src/pages/contact-us/index.tsx
+++ src/pages/contact-us/index.tsx
@@ -3,15 +3,16 @@ import { FaLocationDot } from 'react-icons/fa6'
 
 const ContactUs = () => {
   return (
-    <section className="relative top-0 right-0 left-0 bottom-0 w-full py-20 min-h-screen bg-[#10121D] flex-center gap-5 flex-col ">
-      <h1 className="text-5xl font-bold text-white">Contact Us</h1>
-      <p className="mb-3 text-sm font-normal text-white">Any question or remarks? Just write us a message!</p>
-      <div className="bg-white w-[878px] h-[467px] rounded-md flex p-3 ">
-        <div className='relative bg-[url("/images/contact-us/banner.png")] w-[40%] rounded-md overflow-hidden bg-cover p-8 text-white flex flex-col gap-2 justify-start'>
+    <section className="relative overflow-hidden top-0 right-0 left-0 bottom-0 w-full py-20 min-h-screen bg-[#10121D] flex-center gap-3 md:gap-5 flex-col ">
+      <h1 className="mt-5 text-3xl font-bold text-white md:text-5xl md:mt-0">Contact Us</h1>
+      <p className="mb-3 text-xs font-normal text-white md:text-sm">
+        Any question or remarks? Just write us a message!
+      </p>
+      <div className="bg-transparent md:bg-white w-full md:w-[878px] md:h-[467px] rounded-md flex md:flex-row flex-col-reverse p-3 ">
+        <div className='relative bg-[url("/images/contact-us/banner.png")] h-[357px] md:h-auto w-full md:w-[40%] rounded-md  bg-cover p-8 text-white flex flex-col gap-2 justify-start'>
           <h2 className="text-2xl font-medium">Contact Information</h2>
           <p className="text-sm font-normal text-[#C9C9C9]">Say something to start a live chat!</p>
-
-          <ul className="flex flex-col gap-8 mt-14">
+          <ul className="flex flex-col gap-5 mt-5 md:gap-8 md:mt-14">
             <li className="flex items-center justify-start gap-5">
               <FaPhoneVolume className="w-4 h-4 -rotate-45" /> <p className="text-sm font-light">yestech@yes-led.com</p>
             </li>
@@ -38,9 +39,9 @@ const ContactUs = () => {
             </li>
           </ul>
         </div>
-        <form className="relative w-[60%] p-8 flex flex-col gap-5">
+        <form className="relative w-full  h-[357px] md:h-auto bg-white md:bg-transparent rounded-md md:rounded-none mb-5 md:mb-0 md:w-[60%] p-8 flex flex-col gap-5">
           <div className="flex flex-col gap-2">
-            <label htmlFor="" className="text-[#8D8D8D] font-medium text-lg">
+            <label htmlFor="" className="text-[#8D8D8D] font-medium text-sm md:text-lg">
               Name
             </label>
             <input
@@ -50,7 +51,7 @@ const ContactUs = () => {
             />
           </div>
           <div className="flex flex-col gap-2">
-            <label htmlFor="" className="text-[#8D8D8D] font-medium text-lg">
+            <label htmlFor="" className="text-[#8D8D8D] font-medium text-sm md:text-lg">
               Phone Number
             </label>
             <input
@@ -60,7 +61,7 @@ const ContactUs = () => {
             />
           </div>
           <div className="flex flex-col gap-2">
-            <label htmlFor="" className="text-[#8D8D8D] font-medium text-lg">
+            <label htmlFor="" className="text-[#8D8D8D] font-medium text-sm md:text-lg">
               Email
             </label>
             <input
@@ -71,7 +72,7 @@ const ContactUs = () => {
           </div>
           <button
             type="submit"
-            className="absolute bottom-0 px-12 py-2 font-semibold text-black transition-all border-2 border-black rounded-full cursor-pointer hover:bg-primary hover:border-primary hover:text-white right-8"
+            className="absolute px-12 py-1 font-semibold text-black transition-all border-2 border-black rounded-full cursor-pointer bottom-5 md:bottom-0 md:py-2 hover:bg-primary hover:border-primary hover:text-white right-8"
           >
             Send
           </button>
